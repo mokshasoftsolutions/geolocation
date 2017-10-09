a68cc76bb280826003c369d1d4e6b8e9a71fbb5b
suresh <mokshasoftsolutions@gmail.com>
2017-10-09 17:10:36 +0530

    intial  push

---
+++ traccar-web/web/app/model/Position.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2015 Anton Tananaev (anton@traccar.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Ext.define('Traccar.model.Position', {
+    extend: 'Ext.data.Model',
+    identifier: 'negative',
+
+    fields: [{
+        name: 'id',
+        type: 'int'
+    }, {
+        name: 'protocol',
+        type: 'string'
+    }, {
+        name: 'deviceId',
+        type: 'int'
+    }, {
+        name: 'serverTime',
+        type: 'date',
+        dateFormat: 'c'
+    }, {
+        name: 'deviceTime',
+        type: 'date',
+        dateFormat: 'c'
+    }, {
+        name: 'fixTime',
+        type: 'date',
+        dateFormat: 'c'
+    }, {
+        name: 'valid',
+        type: 'boolean'
+    }, {
+        name: 'accuracy',
+        type: 'float'
+    }, {
+        name: 'latitude',
+        type: 'float'
+    }, {
+        name: 'longitude',
+        type: 'float'
+    }, {
+        name: 'altitude',
+        type: 'float'
+    }, {
+        name: 'speed',
+        type: 'float'
+    }, {
+        name: 'course',
+        type: 'float'
+    }, {
+        name: 'address',
+        type: 'string'
+    }, {
+        name: 'attributes'
+    }, {
+        name: 'speedConverted',
+        type: 'float',
+        calculate: function (data) {
+            return Ext.getStore('SpeedUnits').convertValue(
+                data.speed, Traccar.app.getPreference('speedUnit'));
+        }
+    }, {
+        name: 'distanceConverted',
+        type: 'float',
+        calculate: function (data) {
+            return Ext.getStore('DistanceUnits').convertValue(
+                    data.attributes.distance, Traccar.app.getPreference('distanceUnit'));
+        }
+    }, {
+        name: 'rpm',
+        type: 'float',
+        calculate: function (data) {
+            return data.attributes.rpm;
+        }
+    }, {
+        name: 'fuel',
+        type: 'float',
+        calculate: function (data) {
+            return data.attributes.fuel;
+        }
+    }]
+});
